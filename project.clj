1390bc87bfa4247950c2b4dd94e0562d0b8fad92
Nick Klauer <klauer@gmail.com>
2012-05-15 22:29:38 -0500

    add ccw project files
    the lein-ccw plugin SORT of solves the issue, but with the addition of
    just using the `lein pom` job to generate a pom file, I'll just use
    that to generate an Eclipse Maven project so the darn dependencies
    resolve.

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [ring "1.0.1"]
                  [enlive "1.0.0"] 
                  [cheshire "4.0.0"]]
-  :plugins [[lein-ring "0.7.0"]]
+  :plugins [[lein-ring "0.7.0"]
+            [lein-ccw "1.2.0"]] ;; Only on Leiningen 1.7.x
   :ring {:handler kleptometrics.handler/app}
   :main kleptometrics.app)
--- test/job_librarian/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns job-librarian.core-test
-  (:use clojure.test
-        job-librarian.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
